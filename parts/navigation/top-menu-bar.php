75debf69894ad2e3dc88521bc33741d088ecc8b2
Waqas Ahmed <wakqasahmed@gmail.com>
2016-05-24 11:16:47 +0400

    Removed Change Colors, Blog, and Pages from the toolbar

--- parts/navigation/top-menu-bar.php
+++ parts/navigation/top-menu-bar.php
@@ -4,29 +4,8 @@
         <div class="col-xs-12 col-sm-6 no-margin">
             <ul>
                 <li><a href="<?php echo BASE_URL;?>/index.php?page=home">Home</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle"  data-toggle="dropdown" href="#change-colors">Change Colors</a>
-
-                    <ul class="dropdown-menu" role="menu" >
-                        <li role="presentation"><a role="menuitem" class="changecolor green-text" tabindex="-1" href="#" title="Green color">Green</a></li>
-                        <li role="presentation"><a role="menuitem" class="changecolor blue-text" tabindex="-1" href="#" title="Blue color">Blue</a></li>
-                        <li role="presentation"><a role="menuitem" class="changecolor red-text" tabindex="-1" href="#" title="Red color">Red</a></li>
-                        <li role="presentation"><a role="menuitem" class="changecolor orange-text" tabindex="-1" href="#" title="Orange color">Orange</a></li>
-                        <li role="presentation"><a role="menuitem" class="changecolor navy-text" tabindex="-1" href="#" title="Navy color">Navy</a></li>
-                        <li role="presentation"><a role="menuitem" class="changecolor dark-green-text" tabindex="-1" href="#" title="Darkgreen color">Dark Green</a></li>
-                    </ul>
-                </li>
-                <li><a href="index.php?page=blog">Blog</a></li>
                 <li><a href="index.php?page=faq">FAQ</a></li>
                 <li><a href="index.php?page=contact">Contact</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#pages">Pages</a>
-                    <ul class="dropdown-menu" role="menu">
-                        <?php foreach ( $pages as $key => $packagePage ) : ?>
-                        <li><a href="index.php?page=<?php echo $key;?>&amp;style=<?php echo $_GET['style'];?>"><?php echo $packagePage;?></a></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </li>
             </ul>
         </div><!-- /.col -->
 
